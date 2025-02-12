55197dc3e44774d3b816139699ac8af3d0dc0da0
Gabriel Bartholdy <gabrielbartholdy07@gmail.com>
2025-02-11 22:14:22 -0300

    update ex5
    aa

--- EX5.py
+++ EX5.py
@@ -1,9 +1,12 @@
 #Pergunte ao usuário se está chovendo e converta sua resposta em minúsculas para que não importe em que caso ele digite. Se ele responder "sim", pergunte se está ventando. Se ele responder "sim" a esta segunda pergunta, exiba a resposta "Está ventando muito para um guarda-chuva", caso contrário, exiba a mensagem "Pegue um guarda-chuva". Se ele não respondera sim à primeira pergunta, mostre a resposta "Aproveite o seu dia".
 
-tempo = (input("Esta chovendo?"))
+tempo = input("Esta chovendo?").lower
 
-if "sim".lower():
-    vento = input("Esta ventando?")
-    if "sim":
-        print( "Está ventando muito para um guarda-chuva")
+if tempo=="sim".lower():
+    vento = input("Esta ventando?").lower
+    if vento=="sim".lower():
+        print("Está ventando muito para um guarda-chuva")
     else:
+        print("Pegue um guarda-chuvasi")
+else:
+    print("Aproveite o seu dia")
